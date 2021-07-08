38a5b622cf38f761463404ffa890386cad4e36f1
Altini <altin.m@vitamina-d.agency>
2021-07-09 01:45:16 +0200

    installed composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'af4059684d3026e3ebb4c031423f5047e622e7e1',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'af4059684d3026e3ebb4c031423f5047e622e7e1',
+    ),
+  ),
+);
