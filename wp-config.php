f12a61e38a4670fe16bead093220b42c66a76a4e
Altini <altin.m@vitamina-d.agency>
2021-07-09 02:37:02 +0200

    updated wp-config

--- wp-config.php
+++ wp-config.php
@@ -18,26 +18,17 @@
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
-$url = parse_url(getenv('DATABASE_URL') ? getenv('DATABASE_URL') : getenv('CLEARDB_DATABASE_URL'));
-
-/** The name of the database for WordPress */
-define('DB_NAME', trim($url['path'], '/'));
-
-/** MySQL database username */
-define('DB_USER', $url['user']);
-
-/** MySQL database password */
-define('DB_PASSWORD', $url['pass']);
-
-/** MySQL hostname */
-define('DB_HOST', $url['host']);
-
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+if(isset($_ENV[`CLEARDB_DATABASE_URL`])) {
+    $db = parse_url($_ENV[`CLEARDB_DATABASE_URL`]);
+    define(‘DB_NAME’, trim($db[`path`],`/`));
+    define(‘DB_USER’, $db[`user`]);
+    define(‘DB_PASSWORD’, $db[`pass`]);
+    define(‘DB_HOST’, $db[`host`]);
+    define(‘DB_CHARSET’, ‘utf8’);
+    define(‘DB_COLLATE’, ‘’);
+} else {
+    die(‘No Database credentials!’);
+}
 
 /**#@+
  * Authentication Unique Keys and Salts.
